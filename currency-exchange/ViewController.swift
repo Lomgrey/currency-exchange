9f051a1674d9e80c16c626fdbc3e70473ca755cd
lomgrey <lomgrey.s@gmail.com>
2019-05-05 22:49:26 +0500

    Added double card carousel. Hide keyboard when tapped around.

--- currency-exchange/ViewController.swift
+++ currency-exchange/ViewController.swift
@@ -10,24 +10,40 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var currencyColletionView: UICollectionView!
-    @IBOutlet weak var CurrencyCollectionView2: UICollectionView!
+    @IBOutlet weak var currencyCollectionView: UICollectionView!
+    @IBOutlet weak var currencyCollectionView2: UICollectionView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.hideKeyboardWhenTappedAround()
         
+        setup(collectionView: currencyCollectionView)
+        setup(collectionView: currencyCollectionView2)
+        
+//        let floawLayout = UPCarouselFlowLayout()
+//        floawLayout.itemSize = CGSize(width: UIScreen.main.bounds.size.width - 60.0, height: currencyColletionView.frame.size.height)
+//        floawLayout.scrollDirection = .horizontal
+//        floawLayout.sideItemScale = 0.8
+//        floawLayout.sideItemAlpha = 1.0
+//        floawLayout.spacingMode = .fixed(spacing: 5.0)
+//        currencyColletionView.collectionViewLayout = floawLayout
+//
+//        currencyColletionView.delegate = self
+//        currencyColletionView.dataSource = self
+    }
+    
+    private func setup(collectionView cv: UICollectionView){
         let floawLayout = UPCarouselFlowLayout()
-        floawLayout.itemSize = CGSize(width: UIScreen.main.bounds.size.width - 60.0, height: currencyColletionView.frame.size.height)
+        floawLayout.itemSize = CGSize(width: UIScreen.main.bounds.size.width - 60.0, height: cv.frame.size.height)
         floawLayout.scrollDirection = .horizontal
         floawLayout.sideItemScale = 0.8
         floawLayout.sideItemAlpha = 1.0
         floawLayout.spacingMode = .fixed(spacing: 5.0)
-        currencyColletionView.collectionViewLayout = floawLayout
+        cv.collectionViewLayout = floawLayout
         
-        currencyColletionView.delegate = self
-        currencyColletionView.dataSource = self
+        cv.delegate = self
+        cv.dataSource = self
     }
     
     //MARK: UICollectionViewDelegate and UICollectionViewDataSource
@@ -45,7 +61,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         let cell = cellRaw as! CurrencyCollectionViewCell
         
         cell.currencyNameLabel.text = currencies[indexPath.row]
-        cell.currentBalanceOfThisCurrencyLabel.text = String(100)
+        cell.currentBalanceOfThisCurrencyLabel.text = "You have: 100.00"
         cell.exchangeValueTextField.text = "0"
         
         return cell
@@ -55,7 +71,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         print("item selected == \(indexPath.row)")
     }
     
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
     
-    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
 
