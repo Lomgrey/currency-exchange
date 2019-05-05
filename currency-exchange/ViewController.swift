bf92458cd45a58a7a721674dba09a05a87f3e47b
lomgrey <lomgrey.s@gmail.com>
2019-05-05 11:16:02 +0500

    Initial Commit

---
+++ currency-exchange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  currency-exchange
+//
+//  Created by Sergey Lomakin on 05/05/2019.
+//  Copyright © 2019 Sergey Lomakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
